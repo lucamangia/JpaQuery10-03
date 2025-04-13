b66d34eeb1517def8c4680edd17306d5fbfbb7e9
luca.mangia <lukamangiamele@gmail.com>
2025-04-13 18:15:52 +0200

    aggiunta funzionalità pageable

--- src/main/java/com/exercise/CarApplication/repository/CarRepository.java
+++ src/main/java/com/exercise/CarApplication/repository/CarRepository.java
@@ -1,8 +1,13 @@
 package com.exercise.CarApplication.repository;
 
 import com.exercise.CarApplication.entity.Car;
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.awt.print.Pageable;
+
 @Repository
-public interface CarRepository extends JpaRepository<Car, Long> {}
+public interface CarRepository extends JpaRepository<Car, Long> {
+    Page<Car> findAll(Pageable pageable);
+}
